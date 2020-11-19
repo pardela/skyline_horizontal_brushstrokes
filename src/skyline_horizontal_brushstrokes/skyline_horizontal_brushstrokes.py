5a86b0aae577d50ba13a77517088d5ccfddabc20
pardela <pardela.magnet@gmail.com>
2020-11-19 21:37:52 +0000

    completed algo to count strokes for any combination of building heights

--- src/skyline_horizontal_brushstrokes/skyline_horizontal_brushstrokes.py
+++ src/skyline_horizontal_brushstrokes/skyline_horizontal_brushstrokes.py
@@ -1,2 +1,10 @@
 def count_skyline_horizontal_brushstrokes(input_array):
-    return 0;
+    count_strokes = 0;
+    last_height = 0;
+    for building_height in input_array:
+        if building_height >= last_height:
+            count_strokes = count_strokes + (building_height - last_height);
+        last_height = building_height
+        if count_strokes > 1000000000:
+            return -1;
+    return count_strokes;
--- tests/test_skyline_horizontal_brushstrokes.py
+++ tests/test_skyline_horizontal_brushstrokes.py
@@ -3,3 +3,28 @@ from skyline_horizontal_brushstrokes.skyline_horizontal_brushstrokes import coun
 def test_count_strokes_empty_array():
     assert count_skyline_horizontal_brushstrokes([]) == 0;
 
+def test_count_strokes_one_building():
+    building_height = 5;
+    assert count_skyline_horizontal_brushstrokes([building_height]) == building_height;
+
+def test_count_strokes_two_buidings_together():
+    building1_height = 5;
+    building2_height = 8;
+    assert count_skyline_horizontal_brushstrokes([building1_height, building2_height]) == max(building1_height, building2_height);
+
+def test_count_strokes_same_height_buildings():
+    assert count_skyline_horizontal_brushstrokes([1,1,1,1]) == 1;
+
+def test_count_strokes_different_height_buildings():
+    assert count_skyline_horizontal_brushstrokes([1,3,2,1,2,1,5,3,3,4,2]) == 9;
+
+def test_count_strokes_too_tall_buildings():
+    assert count_skyline_horizontal_brushstrokes([20000000000,3000000000]) == -1;
+
+
+
+
+
+
+
+
